c4cc76d7ef81efa257dbff97c89b3d9b9d6deee0
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2019-04-15 11:26:36 +0200

    Added state for rented, and conditional rendering

--- project/src/Bike.js
+++ project/src/Bike.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
+import * as Bikes from "./Bikes.js";
 import "./css/bike.css";
 
 class Bike extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {rented: false};
+    }
+
+    handleClick() {
+        Bikes.rentBike(0);
+        this.setState({rented: true})
+        console.log(Bikes.getBike(0));
+    }
+
     render() {
-        return (
-            <article className="bicycle">
-                <h2>{this.props.title}</h2>
-                <p>{this.props.city}</p>
-                <p>available from {this.props.startDate} to {this.props.endDate}</p>
-                <p>price: {this.props.price} kr / day</p>
-                <button>Yes please!</button>
-            </article>
-        ); 
+        let article;
+
+        if(this.state.rented) {
+            article = <article className="bicycle">
+                        <h2>This bike is rented</h2>
+                    </article>
+        } 
+        else {
+            article =  <article className="bicycle">
+                        <h2>{this.props.title}</h2>
+                        <p>{this.props.city}</p>
+                        <p>available from {this.props.startDate} to {this.props.endDate}</p>
+                        <p>price: {this.props.price} kr / day</p>
+                        <button onClick={this.handleClick}>
+                        Yes please!
+                        </button>
+                    </article>
+        }
+        return article; 
     }
 }
 
