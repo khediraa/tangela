ba73c48e349aff0fc3d716c7b7f9d3574ef651e9
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2019-04-15 10:53:46 +0200

    Refactored bikes array to array of objects.

--- project/src/Bikes.js
+++ project/src/Bikes.js
@@ -1,7 +1,11 @@
-var bikes = [["Monark Sture","Alingsås","2019/05/01","2019/05/30","50"], 
-            ["Crescent Deca","Örkelljunga","2019/04/04","2019/06/27","70"]];
+export const bikes = [{title: "Monark Sture",
+              city: "Alingsås",
+              startDate: "2019/05/01",
+              endDate: "2019/05/30",
+              price: "50"}, 
+            {title: "Crescent Deca",
+             city: "Örkelljunga",
+             startDate: "2019/04/04",
+             endDate: "2019/06/27",
+             price: "70"}];
 
-document.querySelector(".bicycle > h2").textContent = bikes[0][0];
-document.querySelector(".bicycle > .city").textContent = bikes[0][1];
-document.querySelector(".bicycle > .dates").textContent = "available from " + bikes[0][2] + " to " + bikes[0][3];
-document.querySelector(".bicycle > .price").textContent = bikes[0][4] + " kr / day";
