bb2073f5967b4f4da6165c44374968f6cf9ec090
carcoke <carlos.yechouh@gmail.com>
2019-05-01 19:35:49 +0200

    Adding style to components

--- project/src/ItemListComponent.js
+++ project/src/ItemListComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './css/itemListComponent.css';
 
+/* Function returning component with bikes available in the database */
 function ItemListComponent({title, price}) {
     return (
-        <div>
+        <div className="itemListComponent">
             <h2><a href=""></a>{title}</h2>
-            <h4>{price} kr</h4>
+            <h4>{price} kr/day</h4>
         </div>
     );
 }
