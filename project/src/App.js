e6e5dba417c26a22e66dc99e4a584933dcddceaf
Jesper Berglind <jesper.berglind@gmail.com>
2019-05-03 08:11:40 +0200

    aas

--- project/src/App.js
+++ project/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     render() {
         var bike = BikeHandler.getBike("b1");
         return (
-            <Bike title={bike.name} city={bike.city} startDate={bike.dates[0]} endDate={bike.dates[2]} price={bike.price} />
+            //<Bike title={bike.name} city={bike.city} startDate={bike.dates[0]} endDate={bike.dates[2]} price={bike.price} />
             );
     }
 }
