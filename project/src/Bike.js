de82f2de72877718ea9065393970eab725775d19
ivanlesnukhin <38725308+ivanlesnukhin@users.noreply.github.com>
2019-05-05 13:32:44 +0200

    current date is not visiable

--- project/src/Bike.js
+++ project/src/Bike.js
@@ -3,12 +3,27 @@ import "./css/bike.css";
 import 'react-dates/initialize';
 import * as BikeHandler from "./BikeHandler.js";
 import { DateRangePicker} from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css'; 
+import 'react-dates/lib/css/_datepicker.css';
+//import isInclusivelyAfterDay from '../utils/isInclusivelyAfterDay';
+import * as moment from 'moment';
 class Bike extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
         this.state = { rented: false };
+        //let startDate= this.state.startDate; //11/3/2018
+        //let endDate= this.state.endDate; //11/10/2018
+        //let campaignStartDate = '11/3/2018';
+        //let campaignEndDate= '11/10/2018';
+    }
+
+    /*isBlocked = day => {
+        const availableDates = ["05/17/2019", "05/18/2019", "05/19/2019"] //"2019-08-09", "2019-08-10", "2019-08-11", "2019-08-12"
+        return !availableDates.some(date => day.isSame(date), 'day')
+    }*/
+
+    blocksDay(day) {
+        return day.isSame(moment(), "day");
     }
 
     handleClick() {
@@ -23,7 +38,6 @@ class Bike extends Component {
     handleCalenderChange(startDate, endDate) {
         this.setState({startDate, endDate});
     }
-    
 
     render() {
         let article;
@@ -50,6 +64,9 @@ class Bike extends Component {
                     onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
                     focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                     onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                    //isDayBlocked={this.isBlocked}
+                    isOutsideRange={this.blocksDay}
+
                 />
             </article>
         }
