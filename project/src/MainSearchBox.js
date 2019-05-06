d971672a9eab7642ec499cf86c3d3f239158025b
henrikedstrand <henrik997@icloud.com>
2019-05-06 16:43:59 +0200

    calendar fixed on main search box. minimumNights={0}

--- project/src/MainSearchBox.js
+++ project/src/MainSearchBox.js
@@ -58,6 +58,7 @@ class MainSearchBox extends Component {
         onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
         focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
         onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+        minimumNights={0}
         />
 
         <label>
