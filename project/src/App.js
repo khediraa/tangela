928078a42b4e361122f8c8304c743d12780e4b81
karinsjodin <49153145+karinsjodin@users.noreply.github.com>
2019-05-09 18:15:33 +0200

    Stripe functionality and "fixed" css

--- project/src/App.js
+++ project/src/App.js
@@ -6,28 +6,8 @@ import NoMatch from './NoMatch';
 import BikePage from './BikePage';
 import PaymentPage from './PaymentPage';
 import ConfirmationPage from './ConfirmationPage';
-import {Elements, StripeProvider} from 'react-stripe-elements';
-
-class App extends Component {
-  render() {
-    return (
-      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-        <div className="example">
-          <h1>React Stripe Elements Example</h1>
-          <Elements>
-            <PaymentPage />
-          </Elements>
-        </div>
-      </StripeProvider>
-    );
-  }
-}
-
-export default App;
 
 
-
-/** 
 class App extends Component {
     render() {
         return (
@@ -47,4 +27,3 @@ class App extends Component {
 }
 
 export default App;
-*/
--- project/src/PaymentPage.js
+++ project/src/PaymentPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
-import "./css/default.css";
+import "./css/paymentPage.css";
+import {Elements, StripeProvider} from 'react-stripe-elements';
 
 class PaymentPage extends Component {
     constructor(props) {
@@ -9,21 +10,33 @@ class PaymentPage extends Component {
       this.submit = this.submit.bind(this);
     }
   
-    async submit(ev) {
-              
+    async submit(ev) {  
         this.setState({complete: true});
       }
   
     render() {
-        if (this.state.complete) return <h1>Rental Confirmed</h1>;
+        if (this.state.complete) return (
+          <div class="payment">
+          <h1>Payment by Stripe</h1>
+          <h2>Rental Confirmed</h2>
+          <h3>The code for your bike is 0734</h3>
+          </div>
+        );
       return (
-        <div className="checkout">
-          <p>Would you like to complete the purchase?</p>
+        <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+        <div class="payment">
+        <h1>Payment by Stripe</h1>
+          <Elements>
+            <div className="checkout">
+          <h3>Would you like to complete the rental?</h3>
           <CardElement />
-          <button onClick={this.submit}>Send</button>
+          <button onClick={this.submit}>Rent Bike</button>
+          </div>
+          </Elements>
         </div>
+      </StripeProvider>
       );
     }
   }
-  
-  export default injectStripe(PaymentPage);
+
+  export default PaymentPage;
