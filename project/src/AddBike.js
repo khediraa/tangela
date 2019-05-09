4aecacb03d5eca01c4791ca344e899c44ec8cb93
Jesper Berglind <jesper.berglind@gmail.com>
2019-05-09 09:28:48 +0200

    Created a class for AddBike

---
+++ project/src/AddBike.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import 'react-dates/initialize';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+import {Route, Link} from 'react-router-dom';
+
+class AddBike extends Component{
+    
+}
