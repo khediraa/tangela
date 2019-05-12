6ca157e8654733a8ea469486e9479f2c1e215c40
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2019-05-12 20:38:21 +0200

    Added update database function to server

--- project/server.js
+++ project/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
 
 //Route setup
 app.get('/', (req, res) => {
@@ -15,3 +16,13 @@ app.listen(port, (req, res) => {
 console.log(`server listening on port: ${port}`)
 
  });
+
+const updateDatabase = (data) => {
+  let path = "bikes.json";
+   try {
+     console.log(data, path);
+     //fs.writeFileSync(path, JSON.stringify(data));
+   } catch (error) {
+     console.error(error);
+   }
+}
