0ac93f410ecc06eb00b6b2e292035c793a9f8dbb
karinsjodin <49153145+karinsjodin@users.noreply.github.com>
2019-05-13 15:49:40 +0200

    dates fixed in bikes.json and the correct dates are showing on the bikePage

--- project/src/BikePage.js
+++ project/src/BikePage.js
@@ -6,7 +6,7 @@ class BikePage extends Component {
     render() {
         var bike = BikeHandler.getBike("5");
         return (
-            <Bike id={bike} dates={bike.dates} title={bike.name} city={bike.city} startDate={bike.dates[0]} endDate={bike.dates[2]} price={bike.price} />
+            <Bike id={bike} dates={bike.dates} title={bike.name} city={bike.city} startDate={bike.dates[0]} endDate={bike.dates[bike.dates.length-1]} price={bike.price} />
             );
     }
 }
