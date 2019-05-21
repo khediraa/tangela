e5ac3e5b7ed7b1a47371e67e1b8118803337b70d
karinsjodin <49153145+karinsjodin@users.noreply.github.com>
2019-05-21 16:18:25 +0200

    The information is properly pushed from the bike page to the payment page
    Co-Authored-By: henrikedstrand <henedstr@student.chalmers.se>

--- project/client/src/PaymentPage.js
+++ project/client/src/PaymentPage.js
@@ -4,26 +4,35 @@ import "./css/paymentPage.css";
 import {Elements, StripeProvider} from 'react-stripe-elements';
 import Bike from './Bike';
 import history from './history';
-import PaymentPageComponent from './PaymentPageComponent';
 import * as BikeHandler from './BikeHandler';
 
+/*
+    Function to calculate the total price of the chosen bike and rental period
+*/
+
+function totalPrice(price, startDate, endDate) {
+  var days = BikeHandler.getDates(new Date(startDate),new Date(endDate)).length;
+  //console.log(days);
+  return (
+    price*days
+  )
+}
 
 function PaymentPage(props) {
 
   const [initialized, setInitialized] = useState(false);
     const [bike, setBike] = useState();
+    const {id, startDate, endDate} = props.match.params;
+
     useEffect(() => {
         if(!initialized) {
-            const {id} = props.match.params;
-            console.log(id);  
+
             BikeHandler.getBike(id)
                 .then((json) => {
                     setBike(json);
                 });
 
-
-            setInitialized(true);
-            
+            setInitialized(true);        
 
         }
     })
@@ -32,26 +41,30 @@ function PaymentPage(props) {
       <div>Loading...</div>
     )
     else
+    
     return( (
         <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
           <div class="payment">
             <h1>Payment by Stripe</h1>
-            <Elements>
               <div className="checkout">
-               
-              <h1>{bike.name}</h1>
-              <p>{bike.price}</p>
-              <p>{bike.dates}</p>
+              <div class = 'bikeSummary'>
+              <h3>{bike.name}</h3>
+              <p>Rental period: {startDate} to {endDate}</p>
+              <p>Total price: {totalPrice(bike.price, startDate, endDate)} SEK</p>
+              </div>
 
 
                 <h3>Would you like to complete the rental?</h3>
                 <br/>
-
+                <Elements>
                 <CardElement />
-                
-            <button >Rent Bike</button>
+                </Elements>
+
+            <button>Pay {totalPrice(bike.price, startDate, endDate)} SEK</button>
             </div>
-            </Elements>
+          <p class = 'terms'>Terms & Conditions: <br/>A penalty fee of 1 000 SEK will be applied if the bike is 
+              returned in the wrong location and a fee of 15 000 SEK will be applied for severely
+              damaged or unreturned bikes</p>
           </div>
         </StripeProvider>
       )
