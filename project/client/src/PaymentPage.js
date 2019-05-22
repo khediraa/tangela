c22adaea4150ea84298d635195c772a496ceef1a
henrikedstrand <henedstr@student.chalmers.se>
2019-05-22 16:24:04 +0200

    fixed background on paymentpage

--- project/client/src/PaymentPage.js
+++ project/client/src/PaymentPage.js
@@ -26,7 +26,7 @@ function PaymentPage(props) {
   const [lname, setLname] = useState();
   const [email, setEmail] = useState();
   const [tel, setTel] = useState();
-  const [confirmed, setConfirmed] = useState(true);
+  const [confirmed, setConfirmed] = useState(false);
 
 
   const [initialized, setInitialized] = useState(false);
@@ -56,110 +56,116 @@ function PaymentPage(props) {
     //.then((status) => {
     //if (status == 200) {
     return (
-      <div class="payment">
-        <h1>Rental confirmed</h1>
-        
-        <h2>Rental Confirmed</h2>
-        <div class='logowrapper'>
-        <img align='right' vertical-align='bottom' src={stripelogo} alt="Logo" />
-        </div>
-      </div>
-    )
-    //}
-    //})
-  }
+      <div class='wrapper'>
+        <div class="payment">
+          <div class='textbox'>
 
-
-
-  else
-
-    return ((
-
-      <div class="payment">
-        {/*<h1>Payment by Stripe</h1>*/}
-        <div className="checkout">
-
-          <div class='bikeSummary'>
-            <h3>{bike.name}</h3>
-            <p>Rental period: {startDate} to {endDate}</p>
-            <p>Total price: {totalPrice(bike.price, startDate, endDate)} SEK</p>
+            <h3> Thank you for your business!</h3>
+            <br />
+            <p> You will recive an email with receipt and contact information.</p>
+            </div>
+            <div class='logowrapper'>
+              <img align='right' vertical-align='bottom' src={stripelogo} alt="Logo" />
+            </div>
           </div>
-          <h3>Would you like to complete the rental?</h3>
-          <br />
+        </div>
+        )
+        //}
+        //})
+      }
+    
+    
+    
+      else
+    
+        return ((
+    <div class='wrapper'>
+      <div class="payment">
+          {/*<h1>Payment by Stripe</h1>*/}
+          <div className="checkout">
 
-          {/*  
+            <div class='bikeSummary'>
+              <h3>{bike.name}</h3>
+              <p>Rental period: {startDate} to {endDate}</p>
+              <p>Total price: {totalPrice(bike.price, startDate, endDate)} SEK</p>
+            </div>
+            <h3>Would you like to complete the rental?</h3>
+            <br />
+
+            {/*  
                 FORM WITH THE CARD ELEMENT AND INPUT FIELDS FOR PAYEE INFORMATION 
             */}
 
-          <form onSubmit={setConfirmed}>
+            <form onSubmit={setConfirmed}>
 
-            {/*  
+              {/*  
                     FIRST NAME 
                 */}
-            <table>
-              <tr>
-                <td>
-                  <div id="AddInfo">
-                    <label>
-                      First name:
+              <table>
+                <tr>
+                  <td>
+                    <div id="AddInfo">
+                      <label>
+                        First name:
                                 <input type="text" name="fname" required placeholder="" value={fname} />
-                    </label>
-                  </div>
-                </td>
+                      </label>
+                    </div>
+                  </td>
 
-                {/*  
+                  {/*  
                             LAST NAME 
                         */}
-                <td>
-                  <div id="AddInfo">
-                    <label>
-                      Last name:
+                  <td>
+                    <div id="AddInfo">
+                      <label>
+                        Last name:
                                 <input type="text" name="lname" required placeholder="" value={lname} />
-                    </label>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                {/*  
+                      </label>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  {/*  
                             CELL NUMBER 
                         */}
-                <td>
-                  <div id="AddInfo">
-                    <label>
-                      Cell phone number:
-                                <input type="tel" name="tel" required  minLength="9" maxLength="14" placeholder="Including country code" value={tel} />
-                    </label>
-                  </div>
-                </td>
+                  <td>
+                    <div id="AddInfo">
+                      <label>
+                        Cell phone number:
+                                <input type="tel" name="tel" required minLength="9" maxLength="14" placeholder="Including country code" value={tel} />
+                      </label>
+                    </div>
+                  </td>
 
-                {/*  
+                  {/*  
                             EMAIL ADDRESS 
                         */}
-                <td>
-                  <div id="AddInfo">
-                    <label>
-                      Email address:
+                  <td>
+                    <div id="AddInfo">
+                      <label>
+                        Email address:
                                 <input type="email" name="email" required placeholder="" value={email} />
-                    </label>
-                  </div>
-                </td>
-              </tr>
-            </table>
+                      </label>
+                    </div>
+                  </td>
+                </tr>
+              </table>
 
 
-            <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-              <Elements>
-                <CardElement />
-              </Elements>
-            </StripeProvider>
-            <button type="submit" value="Submit">Pay {totalPrice(bike.price, startDate, endDate)} SEK </button>
-          </form>
-        </div>
-        <p class='terms'>Terms & Conditions: <br />A penalty fee of 1 000 SEK will be applied if the bike is
-          returned in the wrong location and a fee of 15 000 SEK will be applied for severely
+              <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+                <Elements>
+                  <CardElement />
+                </Elements>
+              </StripeProvider>
+              <button type="submit" value="Submit">Pay {totalPrice(bike.price, startDate, endDate)} SEK </button>
+            </form>
+          </div>
+          <p class='terms'>Terms & Conditions: <br />A penalty fee of 1 000 SEK will be applied if the bike is
+            returned in the wrong location and a fee of 15 000 SEK will be applied for severely
         damaged or unreturned bikes</p>
-      </div>
+        </div>
+        </div>
 
-    )
-    )
-} export default PaymentPage;
+        )
+        )
+    } export default PaymentPage;
