a1a856eff0dcf32b96c208ea36cd95847ebc64e1
ivanlesnukhin <38725308+ivanlesnukhin@users.noreply.github.com>
2019-05-24 17:13:35 +0200

    AddBikeContext is added, location can be saved

--- project/client/src/MapForAdding.js
+++ project/client/src/MapForAdding.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState, useContext } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './css/mapContainer.css';
 import {Link} from 'react-router-dom';
 import MapListComponent from './MapListComponent';
+import {AddBikeContext} from './AddBike';
 
-class MapForAdding extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        currentPos: null
-      };
-      this.handleClick = this.handleClick.bind(this);
+function MapForAdding(props) {
+  const {lng, lat, setLat, setLng} = useContext(AddBikeContext);
+  const [currentPos, setCurrentPos] = useState();
+
+   // static MapForAddingContext = AddBikeContext;  
+  
+    function handleClick(e){
+      setLat(e.latlng.lat);
+      setLng(e.latlng.lng);
+      console.log(e.latlng.lat);
+      console.log(e.latlng.lng);
+      setCurrentPos( {
+                    'lat': e.latlng.lat,
+                    'lng': e.latlng.lng
+                   }
+      )
     }
   
-  
-    handleClick(e){
-      this.setState({ currentPos: e.latlng });
-    }
-  
-    render() {
       return (
         <div>
-          <Map center={this.props.center} zoom={this.props.zoom} onClick={this.handleClick}>
+          <Map center={props.center} zoom={props.zoom} onClick={handleClick}>
             <TileLayer
                 url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             />
-            { this.state.currentPos && <Marker position={this.state.currentPos} draggable={true}>
-              <Popup position={this.state.currentPos}>
-                Current location: <pre>{JSON.stringify(this.state.currentPos, null, 2)}</pre>
+            {currentPos && <Marker position={currentPos} draggable={true}>
+              <Popup position={currentPos}>
+                Current location: <pre>{JSON.stringify(currentPos, null, 2)}</pre>
               </Popup>
             </Marker>}
           </Map>
         </div>
       )
-    }
 }
+MapForAdding.contextType = AddBikeContext;
 
 export default MapForAdding;
