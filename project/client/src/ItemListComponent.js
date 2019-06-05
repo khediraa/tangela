71dc8cc49c70514c9cd4647b89846adb9220ab8d
henrikedstrand <henedstr@student.chalmers.se>
2019-06-05 10:00:12 +0200

    removed unused state variable

--- project/client/src/ItemListComponent.js
+++ project/client/src/ItemListComponent.js
@@ -1,12 +1,10 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import './css/itemListComponent.css';
-import {AppContext} from './App';
 import {Link} from 'react-router-dom';
 import Rating from './Rating';
 
 /* Function returning component with bikes available in the database */
 function ItemListComponent({bikeKey, title, price, ratings}) {
-  const state = useContext(AppContext);
   let link = `/BikePage/${bikeKey}`;
   return (
     <div className="itemListComponent">
